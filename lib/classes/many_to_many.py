8fc9865c16cbec33cef11355f7c555c3e4a7b818
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-11-24 14:11:52 +0300

    challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,59 +1,75 @@
+
+
 class Article:
     all = []
 
     def __init__(self, author, magazine, title):
-        if not isinstance(title, str) or not (5 <= len(title) <= 50):
-            raise ValueError("Title must be a string between 5 and 50 characters")
-        self._title = title
+        # check basic title rules
+        if type(title) != str:
+            raise Exception("Title must be string")
+        if len(title) < 5 or len(title) > 50:
+            raise Exception("Title length bad")
+
+        self.title = title
         self.author = author
         self.magazine = magazine
-        Article.all.append(self)
 
-    @property
-    def title(self):
-        return self._title
+        Article.all.append(self)
 
 
 class Author:
     all = []
 
     def __init__(self, name):
-        if not isinstance(name, str) or len(name) == 0:
-            raise ValueError("Author name must be a non-empty string")
-        self._name = name
+        if type(name) != str or name == "":
+            raise Exception("Bad name")
+
+        self.name = name
         Author.all.append(self)
 
-    @property
-    def name(self):
-        return self._name
-
     def articles(self):
-        """Return all Article instances written by this author"""
-        return [a for a in Article.all if a.author == self]
+        # collect articles written by this author
+        arr = []
+        for a in Article.all:
+            if a.author == self:
+                arr.append(a)
+        return arr
 
     def magazines(self):
-        """Return unique magazines this author has written for"""
-        mags = [a.magazine for a in self.articles()]
-        return list(dict.fromkeys(mags))
+        # unique magazines the author wrote for
+        mags = []
+        for a in self.articles():
+            if a.magazine not in mags:
+                mags.append(a.magazine)
+        return mags
 
     def add_article(self, magazine, title):
-        """Creates a new article for this author"""
         return Article(self, magazine, title)
 
     def topic_areas(self):
-        """Return unique topic areas for all magazines the author has written for"""
-        areas = [m.category for m in self.magazines()]
-        return list(dict.fromkeys(areas)) if areas else None
+        # get categories from magazines
+        areas = []
+        for m in self.magazines():
+            if m.category not in areas:
+                areas.append(m.category)
+
+        if len(areas) == 0:
+            return None
+        return areas
 
 
 class Magazine:
     all = []
 
     def __init__(self, name, category):
+        # internal values
         self._name = None
         self._category = None
+
+        # use setters to validate
         self.name = name
         self.category = category
+
         Magazine.all.append(self)
 
     @property
@@ -62,9 +78,10 @@ class Magazine:
 
     @name.setter
     def name(self, value):
-        """Name must be string length 2–16; invalid assignment ignored"""
-        if isinstance(value, str) and 2 <= len(value) <= 16:
+        # only set if valid
+        if type(value) == str and len(value) >= 2 and len(value) <= 16:
             self._name = value
+        # if not valid, ignore and keep old value
 
     @property
     def category(self):
@@ -72,28 +89,51 @@ class Magazine:
 
     @category.setter
     def category(self, value):
-        """Category must be string length >0; invalid assignment ignored"""
-        if isinstance(value, str) and len(value) > 0:
+        if type(value) == str and len(value) > 0:
             self._category = value
+        # ignore invalid values
 
     def articles(self):
-        """Return all articles written for this magazine"""
-        return [a for a in Article.all if a.magazine == self]
+        arr = []
+        for a in Article.all:
+            if a.magazine == self:
+                arr.append(a)
+        return arr
 
     def contributors(self):
-        """Return unique authors who have written for this magazine, or None"""
-        authors = {a.author for a in self.articles()}
-        return list(authors) if authors else None
+        # authors who wrote at least one article
+        authors = []
+        for a in self.articles():
+            if a.author not in authors:
+                authors.append(a.author)
+        if len(authors) == 0:
+            return None
+        return authors
 
     def article_titles(self):
-        """Return list of titles of articles written for this magazine, or None"""
-        titles = [a.title for a in self.articles()]
-        return titles if titles else None
+        # titles of all articles
+        titles = []
+        for a in self.articles():
+            titles.append(a.title)
+        if len(titles) == 0:
+            return None
+        return titles
 
     def contributing_authors(self):
-        """Return authors with more than 2 articles in this magazine, or None"""
-        counts = {}
+        # authors with more than 2 articles
+        count = {}
         for a in self.articles():
-            counts[a.author] = counts.get(a.author, 0) + 1
-        result = [author for author, c in counts.items() if c > 2]
-        return result if result else None
+            auth = a.author
+            if auth not in count:
+                count[auth] = 1
+            else:
+                count[auth] += 1
+
+        result = []
+        for author in count:
+            if count[author] > 2:
+                result.append(author)
+
+        if len(result) == 0:
+            return None
+        return result
